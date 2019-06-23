baf739bc311f69d5c8c1df7e45a36e4cf003c0a7
avnerg <avnerg@perfectomobile.com>
2019-06-23 12:23:38 +0300

    tasks 2 & 3 - winning score prompt and two dice

--- pig-game/app.js
+++ pig-game/app.js
@@ -1,26 +1,29 @@
-var activePlayer, dice, currentScore, previousDice;
+var activePlayer, currentScore, winningScore, previousDice;
+var dice = [0, 0];
 var score = [0, 0];
 
 init();
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
-    dice = Math.ceil((Math.random() * 6));
-    document.querySelector('.dice').src='dice-' + dice + '.png';
-    document.querySelector('.dice').style.display = 'block';
+    dice[0] = Math.ceil((Math.random() * 6));
+    dice[1] = Math.ceil((Math.random() * 6));
+    document.querySelector('.dice1').src='dice-' + dice[0] + '.png';
+    document.querySelector('.dice1').style.display = 'block';
+    document.querySelector('.dice2').src='dice-' + dice[1] + '.png';
+    document.querySelector('.dice2').style.display = 'block';
     
-    if(dice !== 1){
-        if(dice == 6 && previousDice == 6){
+    if(dice[0] !== 1 && dice[1] !== 1){
+        if(dice[0] == 6 && dice[1] == 6){
             score[activePlayer] = 0;
             document.querySelector('#score-' + activePlayer).innerText = score[activePlayer];
             zeroScoreAndSwitchPlayer();
         }
         else{
-            previousDice = dice;
-            currentScore += dice;
+            currentScore += dice[0] + dice[1];
             document.querySelector('#current-' + activePlayer).innerText = currentScore;
         }
     }
-    else if(dice === 1){
+    else{
         zeroScoreAndSwitchPlayer();
     }
     
@@ -29,7 +32,7 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
 document.querySelector('.btn-hold').addEventListener('click', function(){
     score[activePlayer] += currentScore;
     document.querySelector('#score-' + activePlayer).innerText = score[activePlayer];
-    if(score[activePlayer] < 20){
+    if(score[activePlayer] < winningScore){
         zeroScoreAndSwitchPlayer();
     }
     else{
@@ -51,7 +54,8 @@ function init(){
     document.getElementById('score-1').innerText = 0;
     document.getElementById('current-0').innerText = '0';
     document.getElementById('current-1').innerText = '0';
-    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.dice1').style.display = 'none';
+    document.querySelector('.dice2').style.display = 'none';
     
     document.querySelector('.player-0-panel').classList.remove('active');
     document.querySelector('.player-0-panel').classList.remove('active');
@@ -61,6 +65,7 @@ function init(){
     document.querySelector('.btn-roll').disabled = false;
     activePlayer = 0;
     currentScore = 0;
+    winningScore = prompt('Winning Score:');
 }
 function switchPlayer(){
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
@@ -70,7 +75,6 @@ function switchPlayer(){
 
 function zeroScoreAndSwitchPlayer(){
     currentScore = 0;
-    previousDice = 0;
     document.querySelector('#current-' + activePlayer).innerText = currentScore;
     switchPlayer();
 }
